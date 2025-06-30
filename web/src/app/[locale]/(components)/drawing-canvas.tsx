5ca70bdf9622b53c775825ee590d5d86850d7ce9
keit <keit0728.job@gmail.com>
2025-06-30 23:05:35 +0700

    Please make sure that the lines on Layer 1 are always rendered on top of the lines on Layer 2.

--- web/src/app/[locale]/(components)/drawing-canvas.tsx
+++ web/src/app/[locale]/(components)/drawing-canvas.tsx
@@ -38,6 +38,59 @@ export const DrawingCanvas: FC<DrawingCanvasProps> = ({
   onMouseUp,
   onMouseLeave,
 }) => {
+  // Group lines by layer ID and sort layers in reverse order (higher layer ID on top)
+  const linesByLayer = lines.reduce(
+    (acc, line) => {
+      const layerId = (line.layerId || 1).toString();
+      if (!acc[layerId]) {
+        acc[layerId] = [];
+      }
+      acc[layerId].push(line);
+      return acc;
+    },
+    {} as Record<string, DrawingLine[]>,
+  );
+
+  // Sort layer IDs so that higher layer numbers are rendered first (and thus appear below)
+  const sortedLayerIds = Object.keys(linesByLayer).sort((a, b) => {
+    const numA = parseInt(a, 10);
+    const numB = parseInt(b, 10);
+    return numA - numB; // Sort ascending - layer 1 will be rendered last and appear on top
+  });
+
+  // If there are no lines, render empty canvas with just cursor
+  if (sortedLayerIds.length === 0) {
+    return (
+      <Stage
+        width={dimensions.width}
+        height={dimensions.height}
+        onMouseDown={onMouseDown}
+        onMousemove={onMouseMove}
+        onMouseup={onMouseUp}
+        onMouseLeave={onMouseLeave}
+        onTouchStart={onMouseDown}
+        onTouchmove={onMouseMove}
+        onTouchend={onMouseUp}
+        ref={stageRef}
+        style={{
+          cursor:
+            currentTool === "pen" || currentTool === "eraser"
+              ? "none"
+              : "default",
+        }}
+      >
+        <Layer>
+          <CursorDisplay
+            position={cursorPosition}
+            currentTool={currentTool}
+            brushSize={brushSize}
+            brushColor={brushColor}
+          />
+        </Layer>
+      </Stage>
+    );
+  }
+
   return (
     <Stage
       width={dimensions.width}
@@ -57,16 +110,26 @@ export const DrawingCanvas: FC<DrawingCanvasProps> = ({
             : "default",
       }}
     >
+      {sortedLayerIds.reverse().map((layerId) => {
+        if (linesByLayer[layerId] === undefined) {
+          return null;
+        }
+        return (
+          <Layer key={layerId}>
+            {linesByLayer[layerId].map((line) => (
+              <DrawingLineComponent
+                key={line.id}
+                line={line}
+                isSelected={
+                  selectedLineId === line.id ||
+                  selectedLineIds.includes(line.id)
+                }
+              />
+            ))}
+          </Layer>
+        );
+      })}
       <Layer>
-        {lines.map((line) => (
-          <DrawingLineComponent
-            key={line.id}
-            line={line}
-            isSelected={
-              selectedLineId === line.id || selectedLineIds.includes(line.id)
-            }
-          />
-        ))}
         <CursorDisplay
           position={cursorPosition}
           currentTool={currentTool}
